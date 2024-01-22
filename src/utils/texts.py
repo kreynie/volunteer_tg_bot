77f18e3a61578b71fb9c5be401dc26934cdc7c53
kreyNie <kreynie@vk.com>
2024-01-22 07:39:59 +0300

    Minor changes in texts

--- src/utils/texts.py
+++ src/utils/texts.py
@@ -1,20 +1,25 @@
+# Helping center section
 help_text = "Пока нет справки"
 
+# Users section
 add_users_text = "➕ Пользователи"
 remove_users_text = "❌ Пользователь"
 list_users_text = "📃 Список"
 list_shifts_text = "📅 Логи"
+
+# Rules Section
 edit_rules_text = "📃 Редактировать правила"
 add_rule_text = "➕ Правило"
 remove_rule_text = "❌ Правило"
 
-done_text = "Готово ✅"
-
+# Shifts section
 enter_shift = "Зашел 🟢"
 exit_shift = "Вышел 🔴"
 my_shifts = "Мои последние смены 📃"
-list_rules = "Правила модерации ⚠"
+list_rules = "⚠ Правила"
 
+# Utils section
+done_text = "Готово ✅"
 previous_page = "⬅"
 previous_page_action = "previous"
 next_page = "➡"
