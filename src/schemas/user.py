65c1c70e770dca7f9a295bcf2308f1e9b0535a19
kreyNie <kreynie@vk.com>
2024-01-22 07:19:17 +0300

    Added schemas and method to edit users in UsersService

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -4,18 +4,27 @@ from .base import BaseSchema
 
 class UserSchema(BaseSchema):
     telegram_id: int
+    moderator_id: int
     username: str | None
     role_id: int
 
 
 class UserAddSchema(BaseModel):
     telegram_id: int
+    moderator_id: int
     username: str | None
 
 
-class UserEditSchema(BaseModel):
-    telegram_id: int
+class UserUpdateSchema(BaseModel):
+    id: int
+    moderator_id: int
     role_id: int
+
+
+class UserUpdatePartialSchema(BaseModel):
+    id: int
+    moderator_id: int | None = None
+    role_id: int | None = None
 
 
 class UserGetSchema(BaseModel):
--- src/services/users.py
+++ src/services/users.py
@@ -1,5 +1,4 @@
-from src.database.exceptions import EntityAlreadyExists
-from src.schemas.user import UserDeleteSchema, UserGetSchema, UserSchema, UserAddSchema
+import src.schemas.user as schema 
 from src.utils.unitofwork import IUnitOfWork
 
 
@@ -7,7 +6,7 @@ class UsersService:
     def __init__(self, uow: IUnitOfWork):
         self.uow = uow
 
-    async def add_user(self, user: UserAddSchema) -> int:
+    async def add_user(self, user: schema.UserAddSchema) -> int:
         user_dict = user.model_dump()
         async with self.uow:
             should_return = self.uow.users.model.id
@@ -15,7 +14,21 @@ class UsersService:
             await self.uow.commit()
             return returned_user_id
 
-    async def delete_user(self, user: UserDeleteSchema) -> int:
+    async def edit_user(
+            self,
+            user: schema.UserUpdateSchema | schema.UserUpdatePartialSchema,
+    ) -> int:
+        user_dict = user.model_dump(exclude_none=True)
+        async with self.uow:
+            user_id = await self.uow.users.edit_one(
+                data=user_dict,
+                filter_by={"id": user.id},
+                returning=self.uow.users.model.id,
+            )
+            await self.uow.commit()
+            return user_id
+
+    async def delete_user(self, user: schema.UserDeleteSchema) -> int:
         user_dict = user.model_dump(exclude_none=True)
         async with self.uow:
             should_return = self.uow.users.model.id
@@ -23,20 +36,20 @@ class UsersService:
             await self.uow.commit()
             return returned_user_id
 
-    async def get_user(self, user: UserGetSchema) -> UserSchema | None:
+    async def get_user(self, user: schema.UserGetSchema) -> schema.UserSchema | None:
         user_dict = user.model_dump(exclude_none=True)
         async with self.uow:
             found_user = await self.uow.users.find_one(**user_dict)
             return found_user
 
-    async def get_users(self) -> list[UserSchema]:
+    async def get_users(self) -> list[schema.UserSchema]:
         async with self.uow:
             user = await self.uow.users.find_all()
             return user
 
     async def check_rights(self, user_tg_id: int, required_rights: int) -> bool:
         async with self.uow:
-            user = await self.get_user(UserGetSchema(telegram_id=user_tg_id))
+            user = await self.get_user(schema.UserGetSchema(telegram_id=user_tg_id))
             if user is None:
                 return False
             role = await self.uow.roles.find_one(id=user.role_id)
