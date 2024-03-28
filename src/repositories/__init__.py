932486d668464eeee6d30307b6d54d65ca369a98
kreynie <kreynie@vk.com>
2024-03-28 18:09:36 +0300

    :bug: Added missing import in repositories

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -3,9 +3,11 @@ __all__ = (
     "ShiftLogsRepository",
     "ShiftsRepository",
     "UsersRepository",
+    "RulesRepository",
 )
 
 from .roles import RolesRepository
 from .shift_logs import ShiftLogsRepository
 from .shifts import ShiftsRepository
 from .users import UsersRepository
+from .rules import RulesRepository
